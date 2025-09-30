b864ed819c8b7c1c15dd271170009132919901db
Shivam Rai <22shivamrai@gmail.com>
2025-09-30 09:15:56 +0530

    Adding CRUD operations

--- 01_todo-localstorage-starter/01_todo-localstorage/script.js
+++ 01_todo-localstorage-starter/01_todo-localstorage/script.js
@@ -19,12 +19,34 @@ document.addEventListener('DOMContentLoaded', () => {
 
     tasks.push(newTask);
     saveTasks();
+    renderTask(newTask);
     todoInput.value = "";// clear input
     console.log(tasks)
   });
 
   function renderTask(task) {
-    console.log(task);
+    const li = document.createElement('li');
+    li.setAttribute('data-id',task.id);
+    li.innerHTML = `
+    <span>${task.text}</span>
+    <button>delete</button>
+    `;
+
+    li.addEventListener('click',(e) =>{
+      if (e.target.tagName === "BUTTON") return;
+      task.completed = !task.completed;
+      li.classList.toggle("completed");
+      saveTasks();
+    });
+
+    li.querySelector('button').addEventListener('click', (e)=>{
+      e.stopPropagation();
+      tasks = tasks.filter(t => t.id !== task.id);
+      li.remove();
+      saveTasks();
+    })
+
+    todoList.appendChild(li);
   }
 
   function saveTasks() {
